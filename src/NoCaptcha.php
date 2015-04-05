2dd87f251dc0dfbb7b2876b97372413b6d93b7d7
Rahul Kadyan <hi@znck.me>
2015-04-05 23:57:00 +0530

    Use environment proxy to fetch response

--- src/NoCaptcha.php
+++ src/NoCaptcha.php
@@ -2,127 +2,152 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-class NoCaptcha {
+class NoCaptcha
+{
 
-	const CLIENT_API = 'https://www.google.com/recaptcha/api.js';
-	const VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+    const CLIENT_API = 'https://www.google.com/recaptcha/api.js';
+    const VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
 
-	/**
-	 * //
-	 * 
-	 * @var string
-	 */
-	protected $secret;
+    /**
+     * //
+     *
+     * @var string
+     */
+    protected $secret;
 
-	/**
-	 * //
-	 * 
-	 * @var string
-	 */
-	protected $sitekey;
+    /**
+     * //
+     *
+     * @var string
+     */
+    protected $sitekey;
 
-	/**
-	 * //
-	 * 
-	 * @param string $secret
-	 * @param string $sitekey
-	 */
-	public function __construct($secret, $sitekey)
-	{
-		$this->secret = $secret;
-		$this->sitekey = $sitekey;
-	}
+    /**
+     * //
+     *
+     * @param string $secret
+     * @param string $sitekey
+     */
+    public function __construct($secret, $sitekey)
+    {
+        $this->secret = $secret;
+        $this->sitekey = $sitekey;
+    }
 
-	/**
-	 * //
-	 * 
-	 * @return string
-	 */
-	public function display($attributes = [], $lang = null)
-	{
-		$attributes['data-sitekey'] = $this->sitekey;
+    /**
+     * //
+     *
+     * @return string
+     */
+    public function display($attributes = [], $lang = null)
+    {
+        $attributes['data-sitekey'] = $this->sitekey;
 
-		$html  = '<script src="'.$this->getJsLink($lang).'" async defer></script>'."\n";
-		$html .= '<div class="g-recaptcha"'.$this->buildAttributes($attributes).'></div>';
+        $html = '<script src="' . $this->getJsLink($lang) . '" async defer></script>' . "\n";
+        $html .= '<div class="g-recaptcha"' . $this->buildAttributes($attributes) . '></div>';
 
-		return $html;
-	}
+        return $html;
+    }
 
-	/**
-	 * //
-	 * 
-	 * @param  string $response
-	 * @param  string $clientIp
-	 * @return bool
-	 */
-	public function verifyResponse($response, $clientIp = null)
-	{
-		if (empty($response)) return false;
+    /**
+     * //
+     *
+     * @param  string $response
+     * @param  string $clientIp
+     *
+     * @return bool
+     */
+    public function verifyResponse($response, $clientIp = null)
+    {
+        if (empty($response)) return false;
 
-		$response = $this->sendRequestVerify([
-			'secret'   => $this->secret,
-			'response' => $response,
-			'remoteip' => $clientIp
-		]);
+        $response = $this->sendRequestVerify([
+            'secret'   => $this->secret,
+            'response' => $response,
+            'remoteip' => $clientIp
+        ]);
 
-		return isset($response['success']) && $response['success'] === true;
-	}
+        return isset($response['success']) && $response['success'] === true;
+    }
 
-	/**
-	 * //
-	 * 
-	 * @param  Request $request
-	 * @return bool
-	 */
-	public function verifyRequest(Request $request)
-	{
-		return $this->verifyResponse(
-			$request->get('g-recaptcha-response'),
-			$request->getClientIp()
-		);
-	}
+    /**
+     * //
+     *
+     * @param  Request $request
+     *
+     * @return bool
+     */
+    public function verifyRequest(Request $request)
+    {
+        return $this->verifyResponse(
+            $request->get('g-recaptcha-response'),
+            $request->getClientIp()
+        );
+    }
 
-	/**
-	 * //
-	 * 
-	 * @return string
-	 */
-	public function getJsLink($lang = null)
-	{
-		return $lang ? static::CLIENT_API.'?hl='.$lang : static::CLIENT_API;
-	}
+    /**
+     * //
+     *
+     * @return string
+     */
+    public function getJsLink($lang = null)
+    {
+        return $lang ? static::CLIENT_API . '?hl=' . $lang : static::CLIENT_API;
+    }
 
-	/**
-	 * //
-	 * 
-	 * @param  array  $query
-	 * @return array
-	 */
-	protected function sendRequestVerify(array $query = [])
-	{
-		$link = static::VERIFY_URL.'?'.http_build_query($query);
+    /**
+     * //
+     *
+     * @param  array $query
+     *
+     * @return array
+     */
+    protected function sendRequestVerify(array $query = [])
+    {
+        $link = static::VERIFY_URL . '?' . http_build_query($query);
 
-		$response = file_get_contents($link);
+        $proxy_url = env('https_proxy', env('HTTPS_PROXY'));
 
-		return json_decode($response, true);
-	}
+        if ($proxy_url) {
+            $parts = explode('@', substr($proxy_url, 7), 2);
 
-	/**
-	 * //
-	 * 
-	 * @param  array  $attributes
-	 * @return string
-	 */
-	protected function buildAttributes(array $attributes)
-	{
-		$html = [];
+            $options = [
+                'https' => [
+                    'proxy'           => end($parts),
+                    'request_fulluri' => true,
+                ]
+            ];
 
-		foreach ($attributes as $key => $value)
-		{
-			$html[] = $key.'="'.$value.'"';
-		}
+            if (count($parts) == 2) {
+                $options['http']['header'] = base64_decode($parts[0]);
+            }
 
-		return count($html) ? ' '.implode(' ', $html) : '';
-	}
+            $proxyContext = stream_context_create($options);
+
+            $response = file_get_contents($link, true, $proxyContext);
+        } else {
+            $response = file_get_contents($link);
+        }
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * //
+     *
+     * @param  array $attributes
+     *
+     * @return string
+     */
+    protected function buildAttributes(array $attributes)
+    {
+        $html = [];
+
+        foreach ($attributes as $key => $value) {
+            $html[] = $key . '="' . $value . '"';
+        }
+
+        return count($html) ? ' ' . implode(' ', $html) : '';
+    }
 
 }
